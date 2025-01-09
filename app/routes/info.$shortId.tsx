329fc6c42ee50d0a6b71c96d7e6496f677c1906a
ronishg27 <iamrg2060@gmail.com>
2025-01-09 21:42:18 +0545

    add info page

--- app/routes/info.$shortId.tsx
+++ app/routes/info.$shortId.tsx
@@ -1,7 +1,10 @@
-import { redirect, useLoaderData } from "@remix-run/react";
+import { Link, redirect, useLoaderData } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
 // import { useEffect } from "react";
 import { URLInfo } from "~/lib/interface";
 import { getUrlDetails } from "~/utils/dbUtils";
+import { generateInfoUrl, generateShortUrlFromShortId } from "~/utils/urlUtils";
 
 export const loader = async ({ params }: { params: { shortId: string } }) => {
 	const urlDetails = await getUrlDetails(params.shortId);
@@ -11,28 +14,70 @@ export const loader = async ({ params }: { params: { shortId: string } }) => {
 		headers: { "Content-Type": "application/json" },
 	});
 };
+
 const Info = () => {
 	const loaderData = useLoaderData<URLInfo>();
-	// useEffect(() => {}, [loaderData]);
+	const [hostname, setHostname] = useState("");
+	useEffect(() => {
+		setHostname(window.location.hostname);
+	}, [loaderData]);
 
 	return (
-		<div>
-			<h1>Info about your shortened URL</h1>
-			{loaderData.qrCodeSVG ? (
-				<>
-					<p>Original URL: {loaderData.redirectURL}</p>
-					<p>Short ID: {loaderData.shortId}</p>
-					{/* Displaying the QR Code (SVG format) */}
-					<div
-						className="w-40"
-						dangerouslySetInnerHTML={{
-							__html: loaderData.qrCodeSVG,
-						}}
-					/>
-				</>
-			) : (
-				<p>Loading...</p>
-			)}
+		<div className="flex flex-col flex-wrap  items-center">
+			<Card className="w-[500px] justify-center">
+				<CardHeader className="font-semibold text-2xl text-center">
+					Info about your shortened URL
+				</CardHeader>
+				<CardContent className="flex flex-col gap-2">
+					{loaderData.qrCodeSVG ? (
+						<>
+							<p>
+								<span className="font-bold">Original URL: </span>
+								<Link to={`/${loaderData.shortId}`}>
+									<span className="underline">{loaderData.redirectURL}</span>
+								</Link>
+							</p>
+							<p>
+								<span className="font-bold">Short ID: </span>
+								<span>{loaderData.shortId}</span>
+							</p>
+							<p>
+								<span className="font-bold">Short URL: </span>
+								<Link to={`/${loaderData.shortId}`}>
+									<span className="underline">
+										{generateShortUrlFromShortId(hostname, loaderData.shortId)}
+									</span>
+								</Link>
+							</p>
+							<p>
+								<span className="font-bold">Info URL: </span>
+								<Link to={`/info/${loaderData.shortId}`}>
+									<span className="underline">
+										{generateInfoUrl(hostname, loaderData.shortId)}
+									</span>
+								</Link>
+							</p>
+
+							<p>
+								<span className="font-bold">Number of Clicks: </span>
+								<span>{loaderData.visitHistory.length}</span>
+							</p>
+							{/* Displaying the QR Code (SVG format) */}
+							<p>
+								<span className="font-bold">QR for the URL: </span>
+							</p>
+							<div
+								className="w-40"
+								dangerouslySetInnerHTML={{
+									__html: loaderData.qrCodeSVG,
+								}}
+							/>
+						</>
+					) : (
+						<p>Loading...</p>
+					)}
+				</CardContent>
+			</Card>
 		</div>
 	);
 };
--- app/routes/url-dashboard.tsx
+++ app/routes/url-dashboard.tsx
@@ -1,5 +1,4 @@
 import { Form, Link, useLoaderData } from "@remix-run/react";
-import { useEffect } from "react";
 import { Button } from "~/components/ui/button";
 import {
 	AlertDialog,
@@ -43,6 +42,7 @@ export async function action({ request }: { request: Request }) {
 	const formData = await request.formData();
 	const shortId = formData.get("shortId") as string;
 	const deleteAll = formData.get("deleteAll") as string;
+
 	if (deleteAll === "true") {
 		return await deleteAllUrls();
 	}
@@ -53,123 +53,133 @@ export async function action({ request }: { request: Request }) {
 
 const Dashboard = () => {
 	const loaderData = useLoaderData<URLInfo[]>();
-	useEffect(() => {
-		// console.log("loader data: ", loaderData?[0]);
-	}, [loaderData]);
+	// useEffect(() => {
+	// 	// console.log("loader data: ", loaderData?[0]);
+	// }, [loaderData]);
 
 	return (
-		<div className="flex flex-col h-[90vh] justify-center">
-			<Card className="w-1/2 mx-auto px-3 py-5">
+		<div className="flex flex-col justify-center">
+			<Card className="sm:w-[785] mx-auto px-3 py-5 md:w-[70vw]">
 				<CardHeader className="mx-auto font-bold">
 					<CardTitle className="text-3xl">Dashboard</CardTitle>
 					<CardDescription>A list of shortened URL.</CardDescription>
 				</CardHeader>
 				{loaderData ? (
 					<>
-						<Table>
-							<TableHeader>
-								<TableRow>
-									<TableHead className="w-[100px]">SN</TableHead>
-									<TableHead>Short ID</TableHead>
-									<TableHead>Original URL</TableHead>
-									<TableHead className="text-right">Number of Clicks</TableHead>
-									<TableHead>Edit</TableHead>
-								</TableRow>
-							</TableHeader>
-							<TableBody>
-								{loaderData?.map((data, index) => (
-									<TableRow key={data._id}>
-										<TableCell className="font-medium">{index + 1}</TableCell>
-										<TableCell className="underline">
-											<Link to={`/info/${data.shortId}`}>{data.shortId}</Link>
-										</TableCell>
-										<TableCell>{data.redirectURL}</TableCell>
-										<TableCell className="text-right">
-											{data.visitHistory.length}
-										</TableCell>
-										<TableCell>
-											{/* <Button
+						{loaderData.length > 0 ? (
+							<>
+								<Table>
+									<TableHeader>
+										<TableRow>
+											<TableHead className="w-[100px]">SN</TableHead>
+											<TableHead>Short ID</TableHead>
+											<TableHead>Original URL</TableHead>
+											<TableHead className="text-right">
+												Number of Clicks
+											</TableHead>
+											<TableHead>Edit</TableHead>
+										</TableRow>
+									</TableHeader>
+									<TableBody>
+										{loaderData?.map((data, index) => (
+											<TableRow key={data._id}>
+												<TableCell className="font-medium">
+													{index + 1}
+												</TableCell>
+												<TableCell className="underline">
+													<Link to={`/info/${data.shortId}`}>
+														{data.shortId}
+													</Link>
+												</TableCell>
+												<TableCell>{data.redirectURL}</TableCell>
+												<TableCell className="text-right">
+													{data.visitHistory.length}
+												</TableCell>
+												<TableCell>
+													{/* <Button
 											variant={"destructive"}
 											onClick={async () => await handleDelete(data.shortId)}>
 											Delete
 										</Button> */}
-											<AlertDialog>
-												<AlertDialogTrigger asChild>
-													<Button
-														variant={"destructive"}
-														// onClick={async () => await handleDelete(data.shortId)}
-													>
-														Delete
-													</Button>
-												</AlertDialogTrigger>
-												<AlertDialogContent>
-													<AlertDialogHeader>
-														<AlertDialogTitle>
-															Are you sure want to delete?
-														</AlertDialogTitle>
-														<AlertDialogDescription>
-															This action cannot be undone. This will
-															permanently delete your URL details and remove the
-															data from our servers.
-														</AlertDialogDescription>
-													</AlertDialogHeader>
-													<AlertDialogFooter>
-														<AlertDialogCancel>Cancel</AlertDialogCancel>
-														<AlertDialogAction>
-															{/* Continue */}
-															<Form method="post">
-																<input
-																	type="text"
-																	name="shortId"
-																	value={data.shortId}
-																	hidden
-																	readOnly
-																/>
-																<input type="submit" value="Delete" />
-															</Form>
-														</AlertDialogAction>
-													</AlertDialogFooter>
-												</AlertDialogContent>
-											</AlertDialog>
-										</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-						{loaderData.length > 0 ? (
-							<AlertDialog>
-								<AlertDialogTrigger asChild>
-									<Button variant={"destructive"}>Delete all</Button>
-								</AlertDialogTrigger>
-								<AlertDialogContent>
-									<AlertDialogHeader>
-										<AlertDialogTitle>
-											Are you sure want to delete all?
-										</AlertDialogTitle>
-										<AlertDialogDescription>
-											This action cannot be undone. This will permanently delete
-											all the URL details and remove the data from our servers.
-										</AlertDialogDescription>
-									</AlertDialogHeader>
-									<AlertDialogFooter>
-										<AlertDialogCancel>Cancel</AlertDialogCancel>
-										<AlertDialogAction>
-											<Form method="post">
-												<input
-													type="text"
-													name="deleteAll"
-													value={"true"}
-													hidden
-													readOnly
-												/>
-												<input type="submit" value="Delete" />
-											</Form>
-										</AlertDialogAction>
-									</AlertDialogFooter>
-								</AlertDialogContent>
-							</AlertDialog>
+													<AlertDialog>
+														<AlertDialogTrigger asChild>
+															<Button
+																variant={"destructive"}
+																// onClick={async () => await handleDelete(data.shortId)}
+															>
+																Delete
+															</Button>
+														</AlertDialogTrigger>
+														<AlertDialogContent>
+															<AlertDialogHeader>
+																<AlertDialogTitle>
+																	Are you sure want to delete?
+																</AlertDialogTitle>
+																<AlertDialogDescription>
+																	This action cannot be undone. This will
+																	permanently delete your URL details and remove
+																	the data from our servers.
+																</AlertDialogDescription>
+															</AlertDialogHeader>
+															<AlertDialogFooter>
+																<AlertDialogCancel>Cancel</AlertDialogCancel>
+																<AlertDialogAction>
+																	{/* Continue */}
+																	<Form method="post">
+																		<input
+																			type="text"
+																			name="shortId"
+																			value={data.shortId}
+																			hidden
+																			readOnly
+																		/>
+																		<input type="submit" value="Delete" />
+																	</Form>
+																</AlertDialogAction>
+															</AlertDialogFooter>
+														</AlertDialogContent>
+													</AlertDialog>
+												</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+
+								<AlertDialog>
+									<AlertDialogTrigger asChild>
+										<Button variant={"destructive"}>Delete all</Button>
+									</AlertDialogTrigger>
+									<AlertDialogContent>
+										<AlertDialogHeader>
+											<AlertDialogTitle>
+												Are you sure want to delete all?
+											</AlertDialogTitle>
+											<AlertDialogDescription>
+												This action cannot be undone. This will permanently
+												delete all the URL details and remove the data from our
+												servers.
+											</AlertDialogDescription>
+										</AlertDialogHeader>
+										<AlertDialogFooter>
+											<AlertDialogCancel>Cancel</AlertDialogCancel>
+											<AlertDialogAction>
+												<Form method="post">
+													<input
+														type="text"
+														name="deleteAll"
+														value={"true"}
+														hidden
+														readOnly
+													/>
+													<input type="submit" value="Delete" />
+												</Form>
+											</AlertDialogAction>
+										</AlertDialogFooter>
+									</AlertDialogContent>
+								</AlertDialog>
+							</>
 						) : (
-							""
+							"No URL found"
 						)}
 					</>
 				) : (
